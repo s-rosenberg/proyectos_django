45d4823470a3bf736915d05207cc026c63052ec6
s-rosenberg <sbastian.rosenberg@gmail.com>
2020-09-29 20:12:43 -0300

    bbdd con postgresql

--- Tienda_Online/Tienda_Online/settings.py
+++ Tienda_Online/Tienda_Online/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'Tienda_Online.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #SQLITE3 POR DEFECTO
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        # POSTGRESQL
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'articulosclientes',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
 
